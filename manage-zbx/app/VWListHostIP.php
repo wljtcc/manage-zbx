a8c8144466bcb87a2d2a65299026f41477b6cb55
Wellington Luiz Jorge Terrão <wljtcc@gmail.com>
2017-09-20 13:37:14 -0300

    Adicionado as views de banco no código

--- manage-zbx/app/VWListHostIP.php
+++ manage-zbx/app/VWListHostIP.php
@@ -9,3 +9,25 @@ class VWListHostIP extends Model
     //
     protected $table = 'vw_host_status';
 }
+
+/*
+
+## VIEW ##
+
+CREATE OR REPLACE VIEW public.vw_host_status(
+    hostid,
+    host,
+    available,
+    ip)
+AS
+  SELECT h.hostid,
+         h.host,
+         h.available,
+         i.ip
+  FROM hosts h
+       JOIN interface i ON h.hostid = i.hostid
+  WHERE (h.status = ANY (ARRAY [ 0, 1 ])) AND
+        h.flags = 0
+  ORDER BY h.host;
+
+*/
--- manage-zbx/app/VWListHostsValue.php
+++ manage-zbx/app/VWListHostsValue.php
@@ -9,3 +9,37 @@ class VWListHostsValue extends Model
     //
     protected $table = 'vw_listhost_value';
 }
+
+/*
+
+## VIEW ##
+
+CREATE OR REPLACE VIEW public.vw_listhost_value(
+    hostid,
+    host,
+    itemid,
+    name,
+    key_,
+    units,
+    description,
+    value,
+    clock)
+AS
+  SELECT h.hostid,
+         h.host,
+         i.itemid,
+         i.name,
+         i.key_,
+         i.units,
+         i.description,
+         hu.value,
+         hu.clock
+  FROM hosts h
+       JOIN items i ON h.hostid = i.hostid
+       JOIN history_uint hu ON i.itemid = hu.itemid
+  WHERE (h.status = ANY (ARRAY [ 0, 1 ])) AND
+        h.flags = 0 AND
+        i.type = 0
+  ORDER BY hu.clock DESC;
+
+*/
